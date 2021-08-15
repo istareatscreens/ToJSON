72bf37403c71a0c383f249961efe26e7bf23d7d6
istareatscreens <schar138@uottawa.ca>
2021-08-14 23:53:39 -0400

    Add ToArray class to handle creation of arrays
    Add IToString interface to handle ToArray and ToJSON format_value cases
    
    Make ToArray friend class of ToJSON to use format_value methods
    
    Remove unnecessary reassignment of string for add_key method

--- ToJSON.h
+++ ToJSON.h
@@ -1,6 +1,5 @@
 #ifndef TOJSON_H
 #define TOJSON_H
-
 #include <string>
 #include <sstream>
 #include <type_traits>
@@ -9,10 +8,24 @@ namespace ToJSON
 {
     using std::string;
 
-    class ToJSON
+    class IToString
     {
+    public:
+        virtual string to_string() const = 0;
+    };
+
+    class ToJSON : public IToString
+    {
+
     private:
         string json;
+
+        inline string add_key(const string &key)
+        {
+            return (json.empty() ? "" : ",") + format_value(key) + ":";
+        };
+
+    protected:
         template <typename T>
         string format_value(const T &input)
         {
@@ -20,63 +33,56 @@ namespace ToJSON
             if (std::is_arithmetic<T>::value)
             {
                 stream << input;
+                return stream.str();
             }
-            else
-            {
-                stream << "\"" << input << "\"";
-            }
-            return stream.str();
+            return "\"" + input + "\"";
         };
 
         string format_value(const bool &input);
         string format_value(const char &input);
-        string format_value(const ToJSON &input);
         string format_value(const string &input);
-
-        inline string add_key(const string &key)
-        {
-            return ((is_empty()) ? "" : json + ", ") + //check if empty
-                   format_value(key) + " : ";
-        };
+        string format_value(const IToString &input);
 
     public:
         ToJSON();
         ToJSON(const unsigned int length);
+
         template <typename... T>
-        ToJSON &addPropertyArray(const string &key, const T &...values)
+        ToJSON &add_array(const string &key, const T &...values)
         {
-            json = add_key(key) + "[";
-            std::stringstream stream;
+            json += add_key(key) + "[";
+            string array = "";
             for (const auto value : {format_value(values)...})
             {
-                stream << "," << value;
+                array += "," + value;
             }
-            json += (stream.str().empty() ? "" : stream.str().substr(1)) + "]";
+            json += (array.empty() ? "" : array.substr(1)) + "]";
             return *this;
         };
 
         template <typename T>
-        ToJSON &add_property_value(const string &key, const T &value)
+        ToJSON &add(const string &key, const T &value)
         {
-            json = add_key(key) + format_value(value);
+            json += add_key(key) + format_value(value);
             return *this;
         };
 
         ToJSON &add_object(const string &key, const ToJSON &object);
-        string to_string() const;
-        bool is_empty() const;
+        string to_string() const override;
+        bool empty() const;
     };
+
+    ToJSON::ToJSON() : json("") {}
 
     ToJSON::ToJSON(const unsigned int length)
     {
         json.reserve(length);
+        json = "";
     }
-
-    ToJSON::ToJSON() : json("") {}
 
     ToJSON &ToJSON::add_object(const string &key, const ToJSON &object)
     {
-        json = add_key(key) + (object.is_empty() ? "{}" : object.to_string());
+        json += add_key(key) + object.to_string();
         return *this;
     }
 
@@ -85,16 +91,14 @@ namespace ToJSON
         return "{" + json + "}";
     }
 
-    bool ToJSON::is_empty() const
+    bool ToJSON::empty() const
     {
         return json.empty();
     }
 
     string ToJSON::format_value(const string &input)
     {
-        std::stringstream stream;
-        stream << "\"" << input << "\"";
-        return stream.str();
+        return "\"" + input + "\"";
     }
 
     string ToJSON::format_value(const bool &input)
@@ -114,9 +118,67 @@ namespace ToJSON
         return format_value(string{input});
     }
 
-    string ToJSON::format_value(const ToJSON &input)
+    string ToJSON::format_value(const IToString &input)
     {
-        return format_value(input.to_string());
+        return input.to_string();
+    }
+
+    class ToArray : public IToString
+    {
+    private:
+        string array;
+
+        inline string add_comma()
+        {
+            return array.empty() ? "" : ",";
+        };
+
+    public:
+        ToArray();
+        ToArray(const unsigned int length);
+
+        template <typename T>
+        ToArray &add(const T &value)
+        {
+            array += add_comma() + format_value(value);
+            return *this;
+        };
+
+        template <typename... T>
+        ToArray &add_multiple(const T &...values)
+        {
+            array += add_comma();
+            string concat = "";
+            for (const auto value : {ToJSON::format_value(values)...})
+            {
+                concat += "," + value;
+            }
+            add(concat.substr(1));
+            return *this;
+        };
+
+        bool empty() const;
+        string to_string() const override;
+
+        friend class ToJSON;
+    };
+
+    ToArray::ToArray(const unsigned int length)
+    {
+        array.reserve(length);
+        array = "";
+    }
+
+    ToArray::ToArray() : array("") {}
+
+    bool ToArray::empty() const
+    {
+        return array.empty();
+    }
+
+    string ToArray::to_string() const
+    {
+        return "[" + array + "]";
     }
 }
 #endif
