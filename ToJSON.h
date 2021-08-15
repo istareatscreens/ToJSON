f1d8d942371c3d0439f6eb3ba7517d35ddbbf03c
istareatscreens <schar138@uottawa.ca>
2021-08-15 02:16:02 -0400

    Add Testing for ToJSON and fix bugs
    Move ToArray above ToJSON to allow format_value methods to be written
    for both cases
    
    Add tests for add, empty, and add_array ToJSON methods
    
    Add zero parameter add_array method for case when user wants empty array

--- ToJSON.h
+++ ToJSON.h
@@ -11,117 +11,8 @@ namespace ToJSON
     class IToString
     {
     public:
-        virtual string to_string() const = 0;
+        virtual string str() const = 0;
     };
-
-    class ToJSON : public IToString
-    {
-
-    private:
-        string json;
-
-        inline string add_key(const string &key)
-        {
-            return (json.empty() ? "" : ",") + format_value(key) + ":";
-        };
-
-    protected:
-        template <typename T>
-        string format_value(const T &input)
-        {
-            std::stringstream stream;
-            if (std::is_arithmetic<T>::value)
-            {
-                stream << input;
-                return stream.str();
-            }
-            return "\"" + input + "\"";
-        };
-
-        string format_value(const bool &input);
-        string format_value(const char &input);
-        string format_value(const string &input);
-        string format_value(const IToString &input);
-
-    public:
-        ToJSON();
-        ToJSON(const unsigned int length);
-
-        template <typename... T>
-        ToJSON &add_array(const string &key, const T &...values)
-        {
-            json += add_key(key) + "[";
-            string array = "";
-            for (const auto value : {format_value(values)...})
-            {
-                array += "," + value;
-            }
-            json += (array.empty() ? "" : array.substr(1)) + "]";
-            return *this;
-        };
-
-        template <typename T>
-        ToJSON &add(const string &key, const T &value)
-        {
-            json += add_key(key) + format_value(value);
-            return *this;
-        };
-
-        ToJSON &add_object(const string &key, const ToJSON &object);
-        string to_string() const override;
-        bool empty() const;
-    };
-
-    ToJSON::ToJSON() : json("") {}
-
-    ToJSON::ToJSON(const unsigned int length)
-    {
-        json.reserve(length);
-        json = "";
-    }
-
-    ToJSON &ToJSON::add_object(const string &key, const ToJSON &object)
-    {
-        json += add_key(key) + object.to_string();
-        return *this;
-    }
-
-    string ToJSON::to_string() const
-    {
-        return "{" + json + "}";
-    }
-
-    bool ToJSON::empty() const
-    {
-        return json.empty();
-    }
-
-    string ToJSON::format_value(const string &input)
-    {
-        return "\"" + input + "\"";
-    }
-
-    string ToJSON::format_value(const bool &input)
-    {
-        if (input)
-        {
-            return "true";
-        }
-        else
-        {
-            return "false";
-        }
-    }
-
-    string ToJSON::format_value(const char &input)
-    {
-        return format_value(string{input});
-    }
-
-    string ToJSON::format_value(const IToString &input)
-    {
-        return input.to_string();
-    }
 
     class ToArray : public IToString
     {
@@ -149,7 +40,7 @@ namespace ToJSON
         {
             array += add_comma();
             string concat = "";
-            for (const auto value : {ToJSON::format_value(values)...})
+            for (const auto value : {format_value(values)...})
             {
                 concat += "," + value;
             }
@@ -158,7 +49,7 @@ namespace ToJSON
         };
 
         bool empty() const;
-        string to_string() const override;
+        string str() const override;
 
         friend class ToJSON;
     };
@@ -176,9 +67,134 @@ namespace ToJSON
         return array.empty();
     }
 
-    string ToArray::to_string() const
+    string ToArray::str() const
     {
         return "[" + array + "]";
+    }
+
+    class ToJSON : public IToString
+    {
+
+    private:
+        string json;
+
+        inline string add_key(const string &key)
+        {
+            return (json.empty() ? "" : ",") + format_value(key) + ":";
+        };
+
+    protected:
+        template <typename T>
+        string format_value(const T &input)
+        {
+            std::stringstream stream;
+            if (std::is_arithmetic<T>::value)
+            {
+                stream << input;
+            }
+            else
+            {
+                stream << "\"" << input << "\"";
+            }
+            return stream.str();
+        };
+
+        string format_value(const bool &input);
+        string format_value(const char &input);
+        string format_value(const string &input);
+        string format_value(const IToString &input);
+        string format_value(const ToJSON &input);
+        string format_value(const ToArray &input);
+
+    public:
+        ToJSON();
+        ToJSON(const unsigned int length);
+
+        template <typename... T>
+        ToJSON &add_array(const string &key, const T &...values)
+        {
+            json += add_key(key) + "[";
+            string array = "";
+            for (const auto value : {format_value(values)...})
+            {
+                array += "," + value;
+            }
+            json += (array.empty() ? "" : array.substr(1)) + "]";
+            return *this;
+        };
+
+        ToJSON &add_array(const string &key);
+
+        template <typename T>
+        ToJSON &add(const string &key, const T &value)
+        {
+            json += add_key(key) + format_value(value);
+            return *this;
+        };
+
+        string str() const override;
+        bool empty() const;
+    };
+
+    ToJSON::ToJSON() : json("") {}
+
+    ToJSON::ToJSON(const unsigned int length)
+    {
+        json.reserve(length);
+        json = "";
+    }
+
+    string ToJSON::str() const
+    {
+        return "{" + json + "}";
+    }
+
+    bool ToJSON::empty() const
+    {
+        return json.empty();
+    }
+
+    ToJSON &ToJSON::add_array(const string &key)
+    {
+        json += add_key(key) + "[]";
+        return *this;
+    }
+
+    string ToJSON::format_value(const string &input)
+    {
+        return "\"" + input + "\"";
+    }
+
+    string ToJSON::format_value(const bool &input)
+    {
+        if (input)
+        {
+            return "true";
+        }
+        else
+        {
+            return "false";
+        }
+    }
+
+    string ToJSON::format_value(const char &input)
+    {
+        return format_value(string{input});
+    }
+
+    string ToJSON::format_value(const IToString &input)
+    {
+        return input.str();
+    }
+
+    string ToJSON::format_value(const ToJSON &input)
+    {
+        return input.str();
+    }
+
+    string ToJSON::format_value(const ToArray &input)
+    {
+        return input.str();
     }
 }
 #endif
--- test/ToJSONTests.cpp
+++ test/ToJSONTests.cpp
@@ -1,7 +1,58 @@
 #include <gtest/gtest.h>
 #include "../ToJSON.h"
 
-TEST(ToJSONTests, DemonstrateGTestMacros)
+TEST(ToJSONTests, TestEmptyToJSON)
 {
-    EXPECT_TRUE(true);
+    ToJSON::ToJSON obj;
+    ASSERT_TRUE(obj.empty());
+}
+
+TEST(ToJSONTests, TestEmptyToArray)
+{
+    ToJSON::ToArray arr;
+    ASSERT_TRUE(arr.empty());
+}
+
+TEST(ToJSONTests, TestAddToJSON)
+{
+    ToJSON::ToJSON obj;
+    obj.add("str", "value")
+        .add("int", -3)
+        .add("float", 3.3)
+        .add("char", static_cast<char>('a'))
+        .add("unsigned int", static_cast<unsigned int>(33))
+        .add("long", static_cast<long>(1000000))
+        .add("bool", true);
+
+    ASSERT_EQ(obj.str(), "{\"str\":\"value\",\"int\":-3,\"float\":3.3,\"char\":\"a\",\"unsigned int\":33,\"long\":1000000,\"bool\":true}");
+    ASSERT_NE(obj.str(), "{int\":-3,\"float\":3.3,\"char\":\"a\",\"unsigned int\":33,\"long\":1000000}");
+}
+
+TEST(ToJSONTests, TestAddObject)
+{
+    ToJSON::ToJSON obj;
+
+    ToJSON::ToJSON obj2;
+
+    obj2.add("hello", "goodbye");
+
+    ToJSON::ToJSON obj3;
+
+    obj.add("int", -3)
+        .add("obj2", obj2)
+        .add("obj3", obj3)
+        .add("int2", 3)
+        .add("obj3", obj2);
+
+    ASSERT_EQ(obj.str(), "{\"int\":-3,\"obj2\":{\"hello\":\"goodbye\"},\"obj3\":{},\"int2\":3,\"obj3\":{\"hello\":\"goodbye\"}}");
+}
+
+TEST(ToJSONTests, TestAddArray)
+{
+    ToJSON::ToJSON obj;
+    obj.add_array("arr", 1, 3.3, false, "ok")
+        .add("int", 3)
+        .add_array("arr2");
+
+    ASSERT_EQ(obj.str(), "{\"arr\":[1,3.3,false,\"ok\"],\"int\":3,\"arr2\":[]}");
 }
